df08b45137f8b31ec33c185d11424db3cca33506
Shashank Yenurkar <uyshashank@gmail.com>
2019-01-08 23:48:49 +0530

    Maybe final

--- routes/homePage.js
+++ routes/homePage.js
@@ -4,7 +4,7 @@ const cookieFunction = require('../cookie/cookieFunction');
 
 const homePageHandler = (req, res) => {    
     var path = "db/";    
-    var todaysDeals = JSON.parse(fs.readFileSync(path + 'todaysDeals.json'));
+    var todaysDeals = JSON.parse(fs.readFileSync(path + 'todaysdeals.json'));
     var latest = JSON.parse(fs.readFileSync(path + 'latest.json'));
     var recommended = JSON.parse(fs.readFileSync(path + 'recommended.json'));
     
